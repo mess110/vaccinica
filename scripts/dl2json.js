698a52aa82112d23e56029d2e204b3781f2d4f71
Cristian Mircea Messel <mess110@gmail.com>
2021-04-07 22:55:12 +0300

    include april data

--- scripts/dl2json.js
+++ scripts/dl2json.js
@@ -1,36 +1,80 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const excelToJson = require('convert-excel-to-json');
+const excelToJson = require('convert-excel-to-json')
 
-const dowloadsDir = 'downloads';
+// gets the number of months since march (including march)
+const monthsSinceStart = () => {
+  const march = new Date('2021-3-1 UTC')
+  const today = new Date()
+
+  return (today.getFullYear() - march.getFullYear()) * 12
+    - march.getMonth()
+    + today.getMonth()
+    + 1
+}
+
+const dowloadsDir = 'downloads'
 const xlsxFiles = fs.readdirSync(dowloadsDir)
   .filter(f => f.endsWith('.xlsx'))
   .sort()
-  .reverse();
-const latestDownloadedFileName = xlsxFiles[0];
-const latestXlsxPath = path.join(dowloadsDir, latestDownloadedFileName);
+  .reverse()
 
-console.log(`Converting ${latestXlsxPath} to json`);
-const jsonObj = excelToJson({
-  sourceFile: latestXlsxPath,
-  header: {
-    rows: 1
-  },
-  columnToKey: {
-    '*': '{{columnHeader}}'
+const processFile = (fileName) => {
+  const latestDownloadedFileName = fileName
+  const latestXlsxPath = path.join(dowloadsDir, latestDownloadedFileName)
+
+  console.log(`Converting ${latestXlsxPath} to json`)
+  const jsonObj = excelToJson({
+    sourceFile: latestXlsxPath,
+    header: {
+      rows: 1
+    },
+    columnToKey: {
+      '*': '{{columnHeader}}'
+    }
+  })
+
+  const data = JSON.stringify(jsonObj)
+
+  const outputFileName = latestDownloadedFileName.replace(/\.[^.]+$/, '.json')
+  const outputPath = path.join(dowloadsDir, outputFileName)
+  console.log(`Writing ${outputPath}`)
+  fs.writeFileSync(outputPath, data)
+
+  return outputPath
+}
+
+const MAX_MONTHS = 2
+
+let processedFiles = []
+for (let i = 0, len = monthsSinceStart() + 5; i < len; i++) {
+  if (i > MAX_MONTHS - 1) {
+    // stop after 2 months because we can't auto download the "next" month
+    // because we don't know the key. after editing download.sh, increment
+    // the max amount of months to process
+    break
   }
-});
+  processedFiles.push(processFile(xlsxFiles[i]))
+}
 
-const data = JSON.stringify(jsonObj);
+console.log("Merging files:")
+console.log(processedFiles)
 
-const outputFileName = latestDownloadedFileName.replace(/\.[^.]+$/, '.json');
-const outputPath = path.join(dowloadsDir, outputFileName);
-console.log(`Writing ${outputPath}`)
-fs.writeFileSync(outputPath, data);
-
-const latestPath = path.join('docs', 'data', 'latest.json');
-console.log(`Copying ${outputPath} to ${latestPath}`)
-fs.copyFileSync(outputPath, latestPath);
+const latestPath = path.join('docs', 'data', 'latest.json')
+let output = undefined
+for (let fileName of processedFiles) {
+  let temp = JSON.parse(fs.readFileSync(fileName))
+  if (output === undefined) {
+    output = temp;
+  } else {
+    for (let item of temp['ag-grid']) {
+      output['ag-grid'].push(item)
+    }
+  }
+}
+console.log(`Saving merged files to ${latestPath}`)
+const data = JSON.stringify(output)
+fs.writeFileSync(latestPath, data)
--- scripts/download.sh
+++ scripts/download.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env sh
 
 OUTPUT_DIR=downloads
-YESTERDAY=`date +"%d.%m.%Y" -d "yesterday"`
-URL="https://data.gov.ro/dataset/b86a78a3-7f88-4b53-a94f-015082592466/resource/bc19c354-644d-4a24-a26f-512129dbc70d/download/vaccinare-covid19-grupe-risc-01-$YESTERDAY.xlsx"
-echo "Downloading $URL to $OUTPUT_DIR/$YESTERDAY.xlsx"
-curl $URL --output $OUTPUT_DIR/$YESTERDAY.xlsx
+
+download()
+{
+  URL=$1
+  MONTH="$2"
+
+  DOWNLOAD_DATE=`date +"%Y.%m.%d"`
+  NAME=$DOWNLOAD_DATE.$MONTH
+  echo "Downloading $URL to $OUTPUT_DIR/$NAME.xlsx"
+  curl $URL --output $OUTPUT_DIR/$NAME.xlsx
+}
+
+# urls to files on data.gov.ro works in strange ways. The key is actually what
+# counts, the actual filename like "dimineata" is made up. and somehow it works
+# don't scratch your head too much with this..
+download "https://data.gov.ro/dataset/b86a78a3-7f88-4b53-a94f-015082592466/resource/bc19c354-644d-4a24-a26f-512129dbc70d/download/dimineata" "martie"
+download "https://data.gov.ro/dataset/b86a78a3-7f88-4b53-a94f-015082592466/resource/a0eb9ff2-9b97-430c-b285-360cadb55307/download/pe" "aprilie"
+
+# TODO: automatically download april. is that even possible since I don't know
+# the key it will have. Might be possible
